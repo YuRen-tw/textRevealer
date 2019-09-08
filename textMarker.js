a15051c6c12eb2edc51f4efe6a8d9d626ad21fd1
Yu-Ren Pan <YuRen.Pan.0117@gmail.com>
2019-09-09 00:41:39 +0800

    Update. Change the methods and add a new (leading) symbol.

--- textMarker.js
+++ textMarker.js
@@ -13,9 +13,17 @@ function* enumerate(iterable, start=0) {
 
 const SymbolTrie = new Map();
 const SymbolMap = new Map();
+const MarkMap = new Map();
 
-function addSymbol(symbol, item) {
-  SymbolMap.set(symbol, item);
+function mkSymbol(symbol) {
+  if (SymbolMap.has(symbol)) return;
+  SymbolMap.set(symbol, {
+    isSymbol: true,
+    canOpening: false,
+    canClosing: false,
+    multipleClosing: false,
+    content: symbol
+  });
   
   let Trie = SymbolTrie;
   for (let char of symbol) {
@@ -25,8 +33,33 @@ function addSymbol(symbol, item) {
   }
   Trie.set('END', symbol);
 }
+function mkMarkBetween(mark, opening, closing, canClosingMultiple) {
+  if (closing === undefined)
+    closing = opening;
+  
+  mkSymbol(opening);
+  mkSymbol(closing);
+  SymbolMap.get(opening).canOpening = true;
+  SymbolMap.get(closing).canClosing = true;
+  
+  if (canClosingMultiple)
+    SymbolMap.get(closing).multipleClosing = true;
+  
+  if (!MarkMap.has(opening))
+    MarkMap.set(opening, {
+      mark: mark,
+      closedBy: []
+    });
+  MarkMap.get(opening).closedBy.push(closing);
+}
 
-function* textObjGen(charGen, mkTextObj) {
+function mkTextObj(text) {
+  return {
+    isSymbol: false,
+    content: text,
+  };
+}
+function* textObjGen(charGen) {
   let charList = [];
   let branchList = [];
   let charListHeadIdx = 0;
@@ -70,3 +103,66 @@ function* textObjGen(charGen, mkTextObj) {
   yield mkTextObj(charList.join(''));
 }
 
+
+function getMarkList(markAmount) {
+  let result = [];
+  for (let [opening, amount] of markAmount)
+      if (amount > 0)
+        result.push(MarkMap.get(opening).mark);
+  return result
+}
+function addMarkAmount(markAmount, opening, add) {
+  let amount = markAmount.get(opening) || 0;
+  if (add >= 0 || amount >= -add)
+    markAmount.set(opening, amount + add);
+  else
+    markAmount.set(opening, 0);
+}
+
+function mkTextMark(textObj, marks) {
+  return {
+    isSymbol: textObj.isSymbol,
+    content: textObj.content,
+    marks: marks
+  }
+}
+function* textMarkGenerator(charGen) {
+  let markAmount = new Map();
+  let markStack = [];
+  
+  for (let textObj of textObjGen(charGen)) {
+    if (textObj.isSymbol) {
+      let markList = [];
+      let done = false;
+      
+      if (textObj.canClosing) {
+        let closing = textObj.content;
+        let notYetClosedMarkStack = [];
+        while (!done && markStack.length > 0) {
+          let opening = markStack.pop();
+          if (MarkMap.get(opening).closedBy.includes(closing)) {
+            addMarkAmount(markAmount, opening, -1);
+            markList.push(`${MarkMap.get(opening).mark}-end`);
+            if (!textObj.multipleClosing)
+              done = true;
+          } else {
+            notYetClosedMarkStack.unshift(opening);
+          }
+        }
+        markStack = markStack.concat(notYetClosedMarkStack);
+        if (markList.length > 0)
+          done = true;
+      }
+      markList = markList.concat(getMarkList(markAmount));
+      if (!done && textObj.canOpening) {
+        let opening = textObj.content;
+        markStack.push(opening);
+        addMarkAmount(markAmount, opening, 1);
+        markList.push(`${MarkMap.get(opening).mark}-start`);
+      }
+      yield mkTextMark(textObj, markList);
+    } else {
+      yield mkTextMark(textObj, getMarkList(markAmount));
+    }
+  }
+}
--- textPacker.js
+++ textPacker.js
@@ -1,51 +1,46 @@
-const MarkType_HTMLClass = new Map([
+const HTMLClass = new Map([
   ['INIT', 'tag'],
   ['TEXT', ''],
   ['SYMBOL', '-SYMBOL'],
-  ['GROUP', '-G'],
   ['CURSOR', '-CURSOR'],
   ['SELECT', '-SELECT'],
 ]);
+function getHTMLClass(type) {
+  return HTMLClass.get(type) || '';
+}
 
-function addMarkType(type, className) {
-  MarkType_HTMLClass.set(type, className);
-}
-function getMarkClass(type) {
-  return MarkType_HTMLClass.get(type) || '';
-}
-function addMark(symbol, type, on='both', view=undefined) {
-  if (view === undefined)
-    view = symbol;
-  addSymbol(symbol, {
-    type: type,
-    on: on,
-    raw: symbol,
+const SymbolViewMap = new Map();
+
+function setSymbolView(symbol, view) {
+  SymbolViewMap.set(symbol, {
     view: view,
     scale: view.length / symbol.length
   });
 }
-function addMarkBetween(type, openingSymbol, closingSymbol) {
-  if (closingSymbol === undefined) {
-    addMark(openingSymbol, type, on='both');
-  } else {
-    addMark(openingSymbol, type, on='start');
-    addMark(closingSymbol, type, on='end');
+function getSymbolView(symbol) {
+  return SymbolViewMap.get(symbol) || {
+    view: symbol,
+    scale: 1
   }
 }
-function addMarkAfter(type, leadingSymbol) {
-  addMark(leadingSymbol, type, on='lead');
-}
-function mkTextObj(text) {
-  return {
-    type: 'TEXT',
-    raw: text,
-    view: text,
-    scale: 1
-  };
-}
-addMarkType('LINEBREAK', '-BR');
-addMark('\n', 'LINEBREAK', '');
 
+function addSymbol(symbol, view=undefined) {
+  if (view !== undefined)
+    setSymbolView(symbol, view);
+  mkSymbol(symbol);
+}
+function addMarkBetween(mark, opening, closing, openingView, closingView) {
+  if (openingView !== undefined)
+    setSymbolView(opening, openingView);
+  if (closingView !== undefined)
+    setSymbolView(closing, closingView);
+  mkMarkBetween(mark, opening, closing);
+}
+function addMarkAfter(mark, leading, closedBySpace=false) {
+  if (closedBySpace)
+    mkMarkBetween(mark, leading, ' ', true);
+  mkMarkBetween(mark, leading, '\n', true);
+}
 
 function toHTML(str) {
   return (
@@ -58,76 +53,26 @@ function toHTML(str) {
 }
 function mkSpanStr(inner, startIdx, endIdx, scale, ...classList) {
   classList = (classList || []).join(' ');
-  let classString = `class="${getMarkClass('INIT')} ${classList}"`;
+  let classString = `class="${getHTMLClass('INIT')} ${classList}"`;
   let data = (`data-start="${startIdx}" ` +
               `data-end="${endIdx}" ` +
               `data-scale="${scale}"` +
-              `data-content="${toHTML(inner)}"`);
+              `data-content="${toHTML(inner.replace('\n', '\\n'))}"`);
   return `<span ${classString} ${data}>${toHTML(inner)}</span>`;
 }
 
-function addTypeAmount(typeAmount, type, act, add) {
-  let amount = typeAmount.get(`${type}@${act}`) || 0;
-  if (add > 0 || amount >= add)
-    typeAmount.set(`${type}@${act}`, amount + add);
-}
-function updateTypeAmount(typeAmount, currType, on) {
-  let classList = [];
-  if (on === 'end')
-    addTypeAmount(typeAmount, currType, 'normal', -1);
-  for (let [type_act, amount] of typeAmount) {
-    let [type, act] = type_act.split('@');
-    if (amount > 0)
-      classList.push(getMarkClass(type));
-    
-    if (act === 'lead' && amount > 0) {
-      addTypeAmount(typeAmount, type, 'lead', -1);
-      if (currType === 'GROUP' && on === 'start')
-        addTypeAmount(typeAmount, type, 'lock', 1);
-    }
-    if (act === 'lock' && amount > 0 && currType === 'GROUP' && on === 'end')
-        addTypeAmount(typeAmount, type, 'lock', -1);
-  }
-  if (on === 'start')
-    addTypeAmount(typeAmount, currType, 'normal', 1);
-  if (on === 'lead')
-    addTypeAmount(typeAmount, currType, 'lead', 1);
-  return classList;
-}
-
 function* spanStrGenerator(charGen) {
-  let typeAmount = new Map();
   let startIdx = 0;
-  
-  for (let textObj of textObjGen(charGen, mkTextObj)) {
-    let endIdx = startIdx + textObj.raw.length;
-    let spanStr = (...classList) =>
-      mkSpanStr(textObj.view, startIdx, endIdx, textObj.scale, ...classList);
-    
-    if (textObj.type !== 'TEXT') {
-      let on = textObj.on;
-      let act = on === 'lead' ? 'lead' : 'normal';
-      let amount = typeAmount.get(`${textObj.type}@${act}`) || 0;
-      
-      let bound = '';
-      if (on === 'start' ||
-          (on === 'both' && amount < 1))
-        bound = 'start';
-      if ((on === 'end' && amount > 0) ||
-          (on === 'both' && amount > 0))
-        bound = 'end';
-      if (on === 'lead')
-        bound = on;
-      
-      let symbolClass = getMarkClass(textObj.type);
-      let classList = updateTypeAmount(typeAmount, textObj.type, bound);
-      yield spanStr(`${symbolClass}-${bound}`,
-                    getMarkClass('SYMBOL'), ...classList);
+  for (let textMark of textMarkGenerator(charGen)) {
+    let endIdx = startIdx + textMark.content.length;
+    if (textMark.isSymbol) {
+      let symbolView = getSymbolView(textMark.content);
+      yield mkSpanStr(symbolView.view, startIdx, endIdx, symbolView.scale,
+                      getHTMLClass('SYMBOL'), ...textMark.marks);
     } else {
-      let classList = updateTypeAmount(typeAmount);
-      yield spanStr(getMarkClass('TEXT'), ...classList);
+      yield mkSpanStr(textMark.content, startIdx, endIdx, 1,
+                      getHTMLClass('TEXT'), ...textMark.marks);
     }
-    
     startIdx = endIdx;
   }
 }
